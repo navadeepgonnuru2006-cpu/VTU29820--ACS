44400c8ccd9507e6e003a3e6f58e93b28d9c650a
navadeepgonnuru2006-cpu <navadeepgonnuru2006@gmail.com>
2026-09-17 09:34:16 +0530

    Task 17.java

---
+++ UNIT-1/Week 2/Session 9/Task 17.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+    
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        
+        ListNode pointer = head;
+
+        while (pointer != slow) {
+            pointer = pointer.next;
+            slow = slow.next;
+        }
+
+        return pointer;
+    }
+}
