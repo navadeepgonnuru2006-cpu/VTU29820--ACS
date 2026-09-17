3a26c70a4a651b7aed7a763237db26cb7f3b5cd2
navadeepgonnuru2006-cpu <navadeepgonnuru2006@gmail.com>
2026-09-17 09:47:00 +0530

    Task 20.java

---
+++ UNIT-2/Week 3/Session 11/Task 20.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        int[] stack = new int[n];
+        int top = -1;
+
+        for (int i = 0; i < n; i++) {
+            while (top >= 0 && temperatures[i] > temperatures[stack[top]]) {
+                int prev = stack[top--];
+                answer[prev] = i - prev;
+            }
+
+            stack[++top] = i;
+        }
+
+        return answer;
+    }
+}
