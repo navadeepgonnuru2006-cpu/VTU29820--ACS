5166ecf332e1012069bfe6d1874f9daecb5cb06c
navadeepgonnuru2006-cpu <navadeepgonnuru2006@gmail.com>
2026-09-17 09:53:41 +0530

    Task 22.java

---
+++ UNIT-2/Week 3/Session 12/Task 22.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] answer = prices.clone();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    answer[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
