affc43cce7a4d4e61bfb6060a3c3a7b1e01e87ed
navadeepgonnuru2006-cpu <navadeepgonnuru2006@gmail.com>
2026-09-17 09:20:28 +0530

    Task 12.java

---
+++ UNIT-1/Week 2/Session 6/Task 12.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        
+        ListNode secondHalf = reverse(slow);
+
+        
+        ListNode firstHalf = head;
+        ListNode temp = secondHalf;
+
+        while (temp != null) {
+            if (firstHalf.val != temp.val) {
+                return false;
+            }
+
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
